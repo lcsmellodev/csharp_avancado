f0af77657da3283fb51edcf99505eff4ec4ce4f6
Lucas Mello <lcsmellodev@gmail.com>
2023-01-13 09:35:43 -0300

    intro ducao a enums

---
+++ IntroEnums/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using aulaEnums.Entities.Enums;
+
+namespace Enums.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + "," + Moment + "," +Status;
+        }
+
+    }
+}
