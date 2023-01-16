1e7c417cfa3d709c9c55dea561dca61dd0580c71
Lucas Mello <lcsmellodev@gmail.com>
2023-01-15 21:13:15 -0300

    ex1 polimorfismo

---
+++ PolimorphismER1/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace PolimorphismER1.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - $ {Payment().ToString("F2",CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ PolimorphismER1/Program.cs
@@ -0,0 +1,43 @@
+﻿using PolimorphismER1.Entities;
+using System.Globalization;
+
+
+List<Employee> emps = new List<Employee>();
+Console.Write("Enter the number of Employees: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Employee #{i + 1} data: ");
+    Console.Write("Outsourced (y/n)?");
+    string answer = Console.ReadLine();
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Hours: ");
+    int hours = int.Parse(Console.ReadLine());
+    Console.Write("Value Per Hour: ");
+    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    if (answer == "Y" || answer == "y")
+    {
+        Console.Write("Additional Charge: ");
+        double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        emps.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+    }
+    else if (answer == "N" || answer == "n")
+    {
+        emps.Add(new Employee(name, hours, valuePerHour));
+    }
+}
+
+    Console.WriteLine("PAYMENTS:");
+    foreach(Employee item in emps)
+    {
+        Console.WriteLine(item);
+    }
+
+
+
+
+
+
