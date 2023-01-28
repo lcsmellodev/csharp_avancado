0af1ba41c94fde45f237d47c8d3d74e4ab2c94a6
Lucas Mello <lcsmellodev@gmail.com>
2023-01-28 14:45:00 -0300

    funcao printer com threads

---
+++ Threads/Printer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            //lock (this)
+            //{
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(1000);
+                    Console.Write(i + $"{Thread.CurrentThread.Name}, ");
+                }
+                
+                Console.WriteLine();
+            //}
+           
+        }
+    }
+}
--- Threads/Program.cs
+++ Threads/Program.cs
@@ -1,24 +1,18 @@
-﻿static void run()
+﻿using Threads;
+
+Console.WriteLine("======MultiThreads======");
+
+Printer p = new Printer();
+Thread[] threads = new Thread[3];
+
+for(int i = 0; i < 3; i++)
 {
-    for (int i = 0; i < 5; i++)
-    {
-        Console.WriteLine("Thread Atual - " + Thread.CurrentThread.Name + i);
-        Thread.Sleep(1000);
-    }
+    threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
+    threads[i].Name = "ThreadFilha " + i;
+}
+foreach (Thread t in threads)
+{
+    t.Start();
 }
 
-Console.WriteLine("Thread principal iniciada");
-Thread.CurrentThread.Name = "Principal - ";
-
-Thread t1 = new Thread(new ThreadStart(run));
-t1.Name = "Secundária = ";
-t1.Start();
-
-for(int i = 0; i < 5; i++)
-{
-    Console.WriteLine("Thread atual - "+ Thread.CurrentThread.Name + i);
-    Thread.Sleep(1000);
-}
-
-Console.WriteLine("Thread principal finalizada");
-Console.Read();
+Console.ReadLine();
